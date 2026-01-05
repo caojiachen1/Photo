4507b580eac464676545ba360f00a4bde953e9d1
caojiachen1 <caojiachen1@163.com>
2026-01-05 10:39:08 +0800

    Add volume control panel with hover functionality for improved user experience

--- Controls/VlcVideoPlayerControl.xaml.cs
+++ Controls/VlcVideoPlayerControl.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
@@ -9,12 +10,21 @@ namespace Photo.Controls
     public sealed partial class VLCVideoPlayerControl : UserControl
     {
         public VLCVideoPlayerViewModel ViewModel { get; }
+        private DispatcherTimer _volumeTimer;
 
         public VLCVideoPlayerControl()
         {
             this.InitializeComponent();
             ViewModel = new VLCVideoPlayerViewModel(this.DispatcherQueue);
             this.Unloaded += VLCVideoPlayerControl_Unloaded;
+
+            _volumeTimer = new DispatcherTimer();
+            _volumeTimer.Interval = TimeSpan.FromMilliseconds(500);
+            _volumeTimer.Tick += (s, e) =>
+            {
+                VolumePanel.Visibility = Visibility.Collapsed;
+                _volumeTimer.Stop();
+            };
         }
 
         private void VideoView_Initialized(object sender, InitializedEventArgs e)
@@ -30,6 +40,28 @@ namespace Photo.Controls
         private void ContentArea_Tapped(object sender, TappedRoutedEventArgs e)
         {
             ViewModel.ToggleControlsCommand.Execute(null);
+        }
+
+        private void VolumeButton_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            _volumeTimer.Stop();
+            VolumePanel.Visibility = Visibility.Visible;
+        }
+
+        private void VolumeButton_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            _volumeTimer.Start();
+        }
+
+        private void VolumeControl_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            _volumeTimer.Stop();
+            VolumePanel.Visibility = Visibility.Visible;
+        }
+
+        private void VolumeControl_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            _volumeTimer.Start();
         }
     }
 }
